4fa757ac06d87cbc4f95b02caa960a25d08348d0
Michael <michaeldouglasdev@gmail.com>
2020-06-14 01:04:18 -0300

    Initial Commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: "6e9774b418af36adaa58d1005273f12f",
+		expiresIn: "1d",
+	},
+};
---
+++ src/shared/infra/typeorm/database/migrations/1587416527990-CreateAppointments.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class CreateAppointments1587416527990
+	implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<any> {
+		await queryRunner.createTable(
+			new Table({
+				name: "appointments",
+				columns: [
+					{
+						name: "id",
+						type: "varchar",
+						isPrimary: true,
+						generationStrategy: "uuid",
+						default: "uuid_generate_v4()",
+					},
+					{
+						name: "provider",
+						type: "varchar",
+						isNullable: false,
+					},
+					{
+						name: "date",
+						type: "timestamp with time zone",
+						isNullable: false,
+					},
+					{
+						name: "create_at",
+						type: "timestamp",
+						default: "now()",
+					},
+					{
+						name: "updated_at",
+						type: "timestamp",
+						default: "now()",
+					},
+				],
+			})
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<any> {
+		await queryRunner.dropTable("appointments");
+	}
+}
